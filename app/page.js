c942af22eec387ba8d96d259b8463494b5e6cb7c
memphison <jonathandunavant@gmail.com>
2026-08-20 13:00:51 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,28 @@
-import { permanentRedirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  permanentRedirect("https://www.airbnb.com/rooms/36699311");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href =
+        "https://www.airbnb.com/rooms/36699311";
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Georgia, serif",
+      }}
+    >
+      <p>Taking you to Dreamy in Savannah…</p>
+    </main>
+  );
 }
