6bbeecbd7819d6c0c451ebf2564ea8a49f782daa
memphison <jonathandunavant@gmail.com>
2026-08-20 13:00:24 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+
 export const metadata = {
   title: "Dreamy in Savannah",
   description: "A dreamy stay in Savannah, Georgia.",
@@ -6,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
